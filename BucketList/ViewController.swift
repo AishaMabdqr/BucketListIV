907c4f12b63ec6c8567d8b85afb17395e12d7191
Aisha Abdq <Ashmq22@gmail.com>
2021-12-20 01:12:05 +0300

    Initial Commit

---
+++ BucketList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BucketList
+//
+//  Created by A Ab. on 16/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
